ad3ff7a703e946e2e4d8c5555932975534c88da7
WolfFromHeaven <prarthana.meena95@gmail.com>
2017-09-18 13:46:18 +0530

    Add files via upload

---
+++ Utility.h
@@ -0,0 +1,12 @@
+#ifndef _MR_UTILITY
+#define _MR_UTILITY
+
+#include <iostream>
+#include <iomanip>
+#include <utility>
+
+void printKey( const unsigned char* key, unsigned int size );
+bool cmpKey( const unsigned char* keyA, const unsigned char* keyB, unsigned int size );
+bool cmpKeyInverse(const std::pair<unsigned char*,unsigned int> keyl, const std::pair<unsigned char*,unsigned int> keyr, unsigned int size);
+
+#endif
